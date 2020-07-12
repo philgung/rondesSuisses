e121e27a11b21cb7788d42ec3a7c5342c6158656
Philippe Gung <philippe.gung@youscribe.com>
2020-07-12 19:49:24 +0200

    refacto ScoreUnSetDe11 & ScoreUnSetDe21

--- Serveur/Score/ScoreUnSetDe11.ts
+++ Serveur/Score/ScoreUnSetDe11.ts
@@ -1,4 +1,6 @@
 import { IScore } from "./Score";
+import { SetBase } from "./SetBase";
+
 export class ScoreUnSetDe11 implements IScore {
     readonly set: SetDe11Points;
     constructor(setA: number, setB: number) {
@@ -9,39 +11,12 @@ export class ScoreUnSetDe11 implements IScore {
     }
 }
 
-class SetDe11Points{
-    private readonly NombreDePointsMax = 15;
-    private readonly NombreDePointsPourGagnerUnSet = 11;
-
-    estValide(): boolean {
-        if (this.valeurA >= this.NombreDePointsPourGagnerUnSet - 1 && 
-            this.valeurB >= this.NombreDePointsPourGagnerUnSet - 1) 
-            {
-            if (this.valeurA > this.NombreDePointsMax || 
-                this.valeurB > this.NombreDePointsMax) {
-                return false;
-            }
-            if (this.valeurA == this.NombreDePointsMax && 
-                this.valeurB == this.NombreDePointsMax) {
-                return false;
-            }
-            if (this.valeurA == this.NombreDePointsMax || 
-                this.valeurB == this.NombreDePointsMax) {
-                return true;
-            }
-            return Math.abs(this.valeurA - this.valeurB) == 2;
-        }
-        if (this.valeurA == this.NombreDePointsPourGagnerUnSet || 
-            this.valeurB == this.NombreDePointsPourGagnerUnSet){
-            return true;
-        }
-        return false;
-    }
-    readonly valeurA: number;
-    readonly valeurB: number;
+class SetDe11Points extends SetBase{
+    protected readonly NombreDePointsMax = 15;
+    protected readonly NombreDePointsPourGagnerUnSet = 11;
+    
     constructor(valeurA: number, valeurB: number) {
-        this.valeurA = valeurA;
-        this.valeurB = valeurB;
+        super(valeurA, valeurB)
     }
 }
 
---
+++ Serveur/Score/SetBase.ts
@@ -0,0 +1,40 @@
+
+export interface ISet{
+    estValide(): boolean
+}
+
+export abstract class SetBase implements ISet{
+    readonly valeurA: number;
+    readonly valeurB: number;
+    constructor(valeurA: number, valeurB: number) {
+        this.valeurA = valeurA;
+        this.valeurB = valeurB;
+    }
+
+    protected abstract readonly NombreDePointsMax;
+    protected abstract readonly NombreDePointsPourGagnerUnSet;
+    estValide(): boolean {
+        if (this.valeurA >= this.NombreDePointsPourGagnerUnSet - 1 &&
+            this.valeurB >= this.NombreDePointsPourGagnerUnSet - 1) {
+            if (this.valeurA > this.NombreDePointsMax ||
+                this.valeurB > this.NombreDePointsMax) {
+                return false;
+            }
+            if (this.valeurA == this.NombreDePointsMax &&
+                this.valeurB == this.NombreDePointsMax) {
+                return false;
+            }
+            if (this.valeurA == this.NombreDePointsMax ||
+                this.valeurB == this.NombreDePointsMax) {
+                return true;
+            }
+            return Math.abs(this.valeurA - this.valeurB) == 2;
+        }
+        if (this.valeurA == this.NombreDePointsPourGagnerUnSet ||
+            this.valeurB == this.NombreDePointsPourGagnerUnSet) {
+            return true;
+        }
+
+        return false;
+    }
+}
