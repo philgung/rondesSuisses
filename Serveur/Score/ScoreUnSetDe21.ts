f7bb072d4008596b613bc669ae12f5b2e2960817
Philippe Gung <philippe.gung@youscribe.com>
2020-07-12 16:01:19 +0200

    refacto ScoreUnSetDe11 & ScoreUnSetDe21

--- Serveur/Score/ScoreUnSetDe21.ts
+++ Serveur/Score/ScoreUnSetDe21.ts
@@ -1,40 +1,21 @@
+import { SetBase } from "./SetBase";
 import { IScore } from "./Score";
 
 export class ScoreUnSetDe21 implements IScore {
-    readonly valeurA: number;
-    readonly valeurB: number;
-    private readonly NombreDePointsMax = 30;
-
-    private readonly NombreDePointsPourGagnerUnSet = 21;
-
+    readonly set: SetDe21Points;
     constructor(valeurA: number, valeurB: number) {
-        this.valeurA = valeurA;
-        this.valeurB = valeurB;
+        this.set = new SetDe21Points(valeurA, valeurB);
     }
     estValide(): boolean {
-        if (this.valeurA >= this.NombreDePointsPourGagnerUnSet - 1 && 
-            this.valeurB >= this.NombreDePointsPourGagnerUnSet - 1)
-        {
-            if (this.valeurA > this.NombreDePointsMax || 
-                this.valeurB > this.NombreDePointsMax)
-            {
-                return false;
-            }
-            if (this.valeurA == this.NombreDePointsMax && 
-                this.valeurB == this.NombreDePointsMax) {
-                return false;
-            }
-            if (this.valeurA == this.NombreDePointsMax || 
-                this.valeurB == this.NombreDePointsMax) {
-                return true;
-            }
-            return Math.abs(this.valeurA - this.valeurB) == 2;
-        }
-        if (this.valeurA == this.NombreDePointsPourGagnerUnSet || 
-            this.valeurB == this.NombreDePointsPourGagnerUnSet){
-            return true;
-        }
-        
-        return false;
+        return this.set.estValide();
+    }
+}
+
+class SetDe21Points extends SetBase{
+    protected readonly NombreDePointsMax = 30;
+    protected readonly NombreDePointsPourGagnerUnSet = 21;
+
+    constructor(valeurA: number, valeurB: number) {
+        super(valeurA, valeurB)
     }
 }
