2f0db96495b3fb5720dcfa6d60efb206251ee360
Alan Khelifa <alan@khelifa.dev>
2020-07-30 02:21:23 +0200

    Ajout de pkg

---
+++ server.js
@@ -0,0 +1,19 @@
+const path = require('path');
+const { createServer } = require('http');
+const { parse } = require('url');
+const next = require('next');
+const nextConfig = require('./next.config');
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({dev, conf: nextConfig});
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+    createServer((req, res) =>
+        handle(req, res, parse(req.url, true).pathname),
+    ).listen(port, (err) => {
+        if (err) throw err;
+        console.log(`> Ready on http://localhost:${port}`);
+    });
+});
