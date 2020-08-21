237c9f4c9f05dc79f225e623beb1ddd1c05e3075
Philippe Gung <philippe.gung@youscribe.com>
2020-08-21 11:59:33 +0200

    refacto

--- Serveur/Composants/SaisieScore.tsx
+++ Serveur/Composants/SaisieScore.tsx
@@ -1,27 +1,54 @@
 import React from "react";
 import { AffecteTerrain } from "./AffecteTerrain";
 
-export class SaisieScore extends React.Component {
+export class SaisieScore extends React.Component {   
     render(){
-        const numeroDeMatchDansDeroulement = 1;
-        const numeroDeMatchDansEcheancier = 1;
-        const formatTableau = "Ronde Suisse";
-        const nomDuTableau = "SH Pou B";
-        const tour = "T1 Tr 1";
+        const match = 
+        { 
+            numeroDeMatchDansDeroulement : 1,
+            numeroDeMatchDansEcheancier : 1,
+            formatTableau : "Ronde Suisse",
+            nomDuTableau : "SH Pou B",
+            tour : "T1 Tr 1",
+            JoueurA : "LEBRUN Adam - U11-Pou - EBNP",
+            JoueurB : "BELLANGER OLLIVIER Dany - U11-Pou - NBC",
+            HeureDeDebut : new Date(2020, 8, 19, 12, 2, 40),
+            HeureDeFin : undefined,
+
+        }
 
         return (
-        <div>{numeroDeMatchDansDeroulement} {numeroDeMatchDansEcheancier} {formatTableau} {nomDuTableau} {tour} 
-            LEBRUN Adam - U11-Pou - EBNP 
-            <SaisieSet/>
-            BELLANGER OLLIVIER Dany - U11-Pou - NBC
-            <input type="text" maxLength={5} size={5} pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$"/> 
-            <input type="text" maxLength={5} size={5} pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$"/> 
-            <AffecteTerrain />
-            <button>Annuler le résultat du match</button>
-            <button>Abandonner les modifications</button>
-            <button>Enregistrer les modifications</button>
+        <div>
+            <Bloc valeur={match.numeroDeMatchDansDeroulement} /> 
+            <Bloc valeur={match.numeroDeMatchDansEcheancier} /> 
+            <Bloc valeur={match.formatTableau} /> 
+            <Bloc valeur={match.nomDuTableau} />
+            <Bloc valeur={match.tour} />
+            <Bloc valeur={match.JoueurA} />
+            <SaisieSet/> 
+            <Bloc valeur={match.JoueurB} />
+            <div style={{display: 'inline-block', margin: '0 0 0 5px'}}>
+                {/* <Heure valeur={match.HeureDeDebut}/> 
+                <Heure valeur={match.HeureDeFin}/> 
+                 */}
+                <Heure /> 
+                <Heure /> 
+
+            </div>
+            <div style={{display: 'inline-block', margin: '0 0 0 5px'}}>
+                <AffecteTerrain />
+            </div>
+            <div style={{display: 'inline-block', margin: '0 0 0 5px'}}>
+                <button>Annuler le résultat du match</button>
+                <button>Abandonner les modifications</button>
+                <button>Enregistrer les modifications</button>
+            </div>
         </div>);
     }
+}
+
+function Bloc(props: { valeur: React.ReactNode; }){
+    return (<div style={{display: 'inline-block', margin: '0 0 0 5px'}}>{props.valeur}</div>);
 }
 
 class SaisieSet extends React.Component{
@@ -39,23 +66,32 @@ class SaisieSet extends React.Component{
     }
     render(){
         return (
-            <span>
+            <div style={{display: 'inline-block', margin: '0 0 0 5px'}}>
                 <input type="text" name="setA"
-                    minLength={2} 
-                    maxLength={2} 
-                    size={2} 
+                    minLength={2} maxLength={2} size={2} 
                     pattern="[0-9]*"
+                    style={{margin: '0 0 0 5px'}}
                     onChange={this.texteSaisi}/> 
                 <input type="text" name="setB"
-                    minLength={2} 
-                    maxLength={2} 
-                    size={2} 
+                    minLength={2} maxLength={2} size={2} 
                     pattern="[0-9]*"
+                    style={{margin: '0 0 0 5px'}}
                     onChange={this.texteSaisi}/>
-            </span>
+            </div>
         );
     }
-
 }
 
-
+class Heure extends React.Component{
+    private _modeleHeure = "^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$";
+    constructor(props: Readonly<{valeur:Date}>){
+        super(props);
+        this.state = {valeur:undefined};
+    }
+    render(){
+        return (
+            <input type="text" maxLength={5} size={5} pattern={this._modeleHeure}/>
+        );
+        //  value={this.state.valeur}
+    }
+}
